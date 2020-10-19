26cc8af9dd25d2a134208d54c58673af3f0b15b9
shyya showkat <shyxum96@gmail.com>
2020-10-19 23:12:07 +0530

    new

---
+++ index.js
@@ -0,0 +1,19 @@
+function bigImg(x) {
+    x.style.height = "100%";
+    x.style.width = "200px";
+    x.style.margin="12.5%";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "50%";
+    x.style.width = "150px";
+    x.style.marginleft="5%";
+  }
+
+  $(function() {
+    $('marquee').mouseover(function() {
+        $(this).attr('scrollamount',0);
+    }).mouseout(function() {
+         $(this).attr('scrollamount',5);
+    });
+}); 
